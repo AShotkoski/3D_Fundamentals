28511821f2ac23b1a98909e8aba6236f14812319
AShotkoski <shotkoskialex@gmail.com>
2023-01-09 10:32:52 -0800

    Revert "had to remove flush stuff thanks cpp20"
    This reverts commit 7644a650ae84e38dceeedeb3ddb3312fec7adc8a.

--- Engine/Keyboard.cpp
+++ Engine/Keyboard.cpp
@@ -65,12 +65,12 @@ bool Keyboard::CharIsEmpty() const
 
 void Keyboard::FlushKey()
 {
-	//std::swap( keybuffer,std::queue<Event>() );
+	std::swap( keybuffer,std::queue<Event>() );
 }
 
 void Keyboard::FlushChar()
 {
-	//std::swap( charbuffer,std::queue<char>() );
+	std::swap( charbuffer,std::queue<char>() );
 }
 
 void Keyboard::Flush()
